20b34b951f1eaf60ff3d733c9904f894ecfb6768
Beken Sarsenbaev <sbeken6@gmail.com>
2025-07-25 21:36:59 +0300

    fix niri config

--- users/beken/home/packages/niri/default.nix
+++ users/beken/home/packages/niri/default.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }: {
-    home = {
-        packages = with pkgs; [
-            niri
-            libnotify
-            wl-clipboard
-        ];
-        sessionVariables.NIXOS_OZONE_WL = "1";
-    };
+    home.packages = with pkgs; [
+        niri
+        libnotify
+        wl-clipboard
+    ];
 
+    sessionVariables.NIXOS_OZONE_WL = "1";
     xdg.configFile."niri/config.kdl".source = ./config.kdl;
 }
