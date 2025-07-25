705bac5b6f5a9f1532e89ab83cbbe891c1562ef4
Beken Sarsenbaev <sbeken6@gmail.com>
2025-07-25 22:32:01 +0300

    fix homemanager config

--- users/beken/home/default.nix
+++ users/beken/home/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, username, ... }: {
     imports = [ ./packages ];
 
-    home-manager = {
+    home-manager.home = {
         inherit username;
         homeDirectory = "/home/${username}";
         stateVersion = "25.05";
