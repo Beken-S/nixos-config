75ee940dcf130d4ed4357cea6011b96f86a7e26a
Beken Sarsenbaev <sbeken6@gmail.com>
2025-07-25 21:25:52 +0300

    fix disko-config

--- flake.nix
+++ flake.nix
@@ -13,11 +13,12 @@
         };
     };
 
-    outputs = { self, nixpkgs, ... }@inputs: {
+    outputs = { self, nixpkgs, disko, ... }@inputs: {
         nixosConfigurations.thinkpad-t510 = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             specialArgs = { inherit inputs; };
             modules = [
+                disko.nixosModules.disko
                 ./hosts/thinkpad-t510
                 ./users/beken
             ];
