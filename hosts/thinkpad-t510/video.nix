64ef3885ddaf101c7bdd32e361968ccd400b5a8f
Beken Sarsenbaev <sbeken6@gmail.com>
2025-07-26 13:12:37 +0300

    fix nvidia drivers

--- hosts/thinkpad-t510/video.nix
+++ hosts/thinkpad-t510/video.nix
@@ -1,23 +1,24 @@
 { config, pkgs, ... }: {
-    hardware.graphics = {
-        enable = true;
-        extraPackages = with pkgs; [
-            nvidia-vaapi-driver
-            ocl-icd
-        ];
-    };
     services.xserver.videoDrivers = [ "nvidia" ];
-
-    hardware.nvidia = {
-        package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
-        modesetting.enable = true;
-        open = false;
-        nvidiaSettings = true;
-    };
 
     config = {
         nix.nvidia.acceptLicense = true;
         boot.blacklistedKernelModules = [ "nouveau" ];
         boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+
+        hardware.graphics = {
+            enable = true;
+            extraPackages = with pkgs; [
+                nvidia-vaapi-driver
+                ocl-icd
+            ];
+        };
+
+        hardware.nvidia = {
+            package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
+            modesetting.enable = true;
+            open = false;
+            nvidiaSettings = true;
+        };
     };
 }
