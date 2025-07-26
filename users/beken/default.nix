cfd385207ec51ab738378494bf78c8d55de74db6
Beken Sarsenbaev <sbeken6@gmail.com>
2025-07-26 23:12:50 +0300

    fix nvidia drivers

--- users/beken/default.nix
+++ users/beken/default.nix
@@ -2,23 +2,21 @@
 let
     username = "beken";
 in {
-    config = {
-        nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowUnfree = true;
 
-        users.users.${username} = {
-            isNormalUser = true;
-            extraGroups = [ "wheel" "networkmanager" ];
-        };
+    users.users.${username} = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" "networkmanager" ];
+    };
 
-        home-manager.users.${username} = {
-            imports = [ ./packages ];
+    home-manager.users.${username} = {
+        imports = [ ./packages ];
 
-            home = {
-                inherit username;
-                homeDirectory = "/home/${username}";
-                sessionVariables.NIXOS_OZONE_WL = "1";
-                stateVersion = "25.05";
-            };
+        home = {
+            inherit username;
+            homeDirectory = "/home/${username}";
+            sessionVariables.NIXOS_OZONE_WL = "1";
+            stateVersion = "25.05";
         };
     };
 }
