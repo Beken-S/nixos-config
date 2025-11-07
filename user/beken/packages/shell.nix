bf6d5d24a7deb0b2106e5d46433256df416b2cae
BekenSarsenbaev <sbeken6@gmail.com>
2025-11-07 20:20:18 +0300

    simplify shell prompt

--- user/beken/packages/shell.nix
+++ user/beken/packages/shell.nix
@@ -1,42 +1,35 @@
 { config, pkgs, lib, ... }: {
     programs.bash.enable = true;
 
-    programs.starship = let
-        divider = fg: bg: "[](bg:${bg} fg:${fg})";
-    in {
+    programs.starship = {
         enable = true;
         enableBashIntegration = true;
         settings = {
-            add_newline = true;
+            add_newline = false;
             format = lib.concatStrings [
-                "[](cyan)"
                 "$directory"
-                (divider "cyan" "red")
                 "$git_branch"
-                (divider "red" "yellow")
                 "$git_commit"
-                (divider "yellow" "bright-black")
                 "$git_status"
-                (divider "bright-black" "black")
                 " "
             ];
             directory = {
-                style = "bg:cyan fg:black" ;
-                format = "[$path ]($style)";
+                style = "bold cyan" ;
+                format = "[$path]($style)";
                 truncation_symbol ="…/";
             };
             git_branch = {
                 symbol = "󰘬 ";
-                style = "bg:red fg:black";
-                format = "[ $symbol$branch ]($style)";
+                style = "bold red";
+                format = "[ $symbol$branch]($style)";
             };
             git_commit = {
-                style = "bg:yellow fg:black";
-                format = "[ $hash ]($style)";
+                style = "bold yellow";
+                format = "[ $hash]($style)";
             };
             git_status = {
-                style = "bg:bright-black fg:black";
-                format = "[( $modified$staged )]($style)";
+                style = "red";
+                format = "[( \\[$modified$staged\\])]($style)";
                 modified = "!";
                 staged = "+";
             };
