86cfd1ae1be122964788610f6ce608127829d83f
Beken Sarsenbaev <sbeken6@gmail.com>
2025-12-09 19:51:58 +0300

    add colorcolumn

--- user/beken/packages/nvim/lua/core.lua
+++ user/beken/packages/nvim/lua/core.lua
@@ -11,6 +11,9 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
+-- Максимальная длина строки
+vim.opt.colorcolumn = "80"
+
 -- Отображение пробельных символов
 vim.opt.list = true
 vim.opt.listchars = {
